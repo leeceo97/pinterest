3546684358b9ce2b02ce2f0e43021bd7993f803b
leeceo97 <leeceo97@naver.com>
2021-07-07 23:59:31 +0900

    protocol test

--- accounts/views.py
+++ accounts/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, "accounts/helloworld.html")
+    if request.method == "POST":
+        return render(request, 'accounts/helloworld.html', context={'text':'POST METHOD!'})
+    else:
+        return render(request, 'accounts/helloworld.html', context={'text':'get METHOD!'})
