f5141978dfed0446e4007846055b7e3bda79f07b
leeceo97 <leeceo97@naver.com>
2021-07-27 00:32:10 +0900

    (feat)create view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, AccountCreateView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path('hello/', hello_world, name='hello_world')
+    path('hello/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
